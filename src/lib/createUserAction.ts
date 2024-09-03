638cbe70fe47ab5fc971f85c2f306be16468c957
Tarek Elkanaria <tarekelkanaria@gmail.com>
2024-09-03 11:50:14 +0300

    Rename validations file

--- src/lib/createUserAction.ts
+++ src/lib/createUserAction.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { redirect } from "next/navigation";
-import { validateUser } from "src/utils/loginValidation";
+import { validateUser } from "src/utils/loginValidations";
 import { cookies } from "next/headers";
 
 export async function createUserAction(_prevState: any, formData: FormData) {
